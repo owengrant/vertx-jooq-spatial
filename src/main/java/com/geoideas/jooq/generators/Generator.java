f6d68f2294cf57a5acd830b5ae12f4e20cfe602b
Owen Grant <owen.dalmon.grant@gmail.com>
2020-06-27 10:54:55 -0400

    add support for JsonObject

--- src/main/java/com/geoideas/jooq/generators/Generator.java
+++ src/main/java/com/geoideas/jooq/generators/Generator.java
@@ -1,13 +1,14 @@
 package com.geoideas.jooq.generators;
 
-import io.github.jklingsporn.vertx.jooq.generate.classic.ClassicAsyncVertxGenerator;
+import io.github.jklingsporn.vertx.jooq.generate.classic.ClassicReactiveVertxGenerator;
 
 import java.time.LocalDateTime;
 import org.jooq.codegen.JavaWriter;
 import org.jooq.meta.TypedElementDefinition;
 import com.geoideas.jooq.types.Geography;
+import io.vertx.core.json.JsonObject;
 
-public class Generator extends ClassicAsyncVertxGenerator {
+public class Generator extends ClassicReactiveVertxGenerator {
     private final String DATETYPE = "java.lang.LocalDate";
      @Override
     protected boolean handleCustomTypeFromJson(TypedElementDefinition<?> column, String setter, String columnType, String javaMemberName, JavaWriter out) {
@@ -21,6 +22,10 @@ public class Generator extends ClassicAsyncVertxGenerator {
          }
         else if(isType(columnType, Geography.class)){
             out.tab(2).println("%s(json.getJsonArray(\"%s\")==null?null: new Geography().fromJson(json.getJsonArray(\"%s\")));", setter, javaMemberName, javaMemberName);
+            return true;
+        }
+        else if(isType(columnType, JsonObject.class)){
+            out.tab(2).println("%s(json.getJsonObject(\"%s\")==null?null: json.getJsonObject(\"%s\"));", setter, javaMemberName, javaMemberName);
             return true;
         }
         return super.handleCustomTypeFromJson(column, setter, columnType, javaMemberName, out);
@@ -38,6 +43,10 @@ public class Generator extends ClassicAsyncVertxGenerator {
         }
         else if(isType(columnType, Geography.class)) {
             out.tab(2).println("json.put(\"%s\",%s()==null?null:%s().toJson());", getJsonKeyName(column), getter, getter);
+            return true;
+        }
+        else if(isType(columnType, JsonObject.class)) {
+            out.tab(2).println("json.put(\"%s\",%s());", getJsonKeyName(column), getter);
             return true;
         }
         return super.handleCustomTypeToJson(column, getter, columnType, javaMemberName, out);
